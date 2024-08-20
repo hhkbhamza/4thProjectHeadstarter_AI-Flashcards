c6da7f49e4174064a2ebbeb6b8876e7b0e824ebc
Amir Hamza <a.hamza8596@gmail.com>
2024-08-19 23:55:22 -0400

    Added helper functions for flashcard generation

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,6 +1,8 @@
 'use client'
 import { useRouter } from "next/navigation"
 import { useUser } from "@clerk/nextjs"
+import { P } from "@clerk/clerk-react/dist/controlComponents-B9SlJ0L1"
+import { collection } from "firebase/firestore"
 
 export default function Generate() {
     const {isLoaded, isSignedIn, user} = useUser()
@@ -15,6 +17,57 @@ export default function Generate() {
         fetch('api/generate', {
             method: 'POST',
             body: text,
+        }).then((res)=> res.json())
+        .then((data) > setFlashcards(data))
+    }
+
+    const handleCardClick = (id) => {
+        setFlipped((prev) => {
+            ...prev,
+            [id]: !prev[id],
         })
     }
+
+    const handleOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const saveFlashcards = async () => {
+        if (!name) {
+            alert('Please enter a name')
+            return
+        }
+
+        const batch = writeBatch(db)
+        const userDocRef = doc(collection(db,'users'), user.id)
+        const docSnap = await getDoc(userDocRef)
+
+        if(docSnap.exists()) {
+            const collections = docSnap.data().flashcards || []
+            if (collections.find((f)=> f.name === name)) }
+            alert("Flashcard collections with the same name already exists.")
+            return
+        }
+        else {
+            collections.push({ name});
+            batch.set(userDocRef, {flashcards: collections}, {merge: true})
+        }
+    }
+    else {
+        batch.set(userDocRef, {flashcards: [{name}]})
+    }
+
+    const colRef = collection(userDocRef, name) 
+    flashcards.forEach((flashcard) => {
+        const cardDocRef = doc(colRef)
+        batch.set(cardDocRef, flashcard)
+    })
+    await batch.commit()
+    handleClose()
+    router.push('/flashcards')
+}
 }
